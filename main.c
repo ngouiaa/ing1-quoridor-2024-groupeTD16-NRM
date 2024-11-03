d2b14e213c957ea20c8e6d306df85282e0958c2e
stnmateo <mateo.stengelin@edu.ece.fr>
2024-11-03 21:58:27 +0100

    main.c

--- main.c
+++ main.c
@@ -2,6 +2,45 @@
 #include <stdlib.h>
 #include "tableauex.h"
 
+int main() {
+    int choix, x;
+    do {
+        printf("\n1 - Lancer une nouvelle partie\n");
+        printf("2 - Reprendre une partie sauvergardée\n");
+        printf("3 - Afficher l'aide\n");
+        printf("4 - Afficher les scores des joueurs\n");
+        printf("5 - Quitter le jeu\n");
+        printf("\n\tChoisir une des options :");
+        scanf("%d",&choix);
+        switch(choix) {
+            case 1 :
+                
+            break;
+            case 2 :
+                
+            break;
+            case 3 :
+                
+            break;
+            case 4 :
+                
+            break;
+            case 5 :
+                printf("Au revoir !\n");
+            break;
+            default :
+                printf("choix non valide !\n");
+        }
+    }while(choix != 5);
+    return 0;
+}
+
+
+
+
+
+
+
 
 int main(void) {
     int p1[T][T];
