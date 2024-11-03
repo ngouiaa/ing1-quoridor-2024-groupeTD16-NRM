de4bf88646ece1189e838b5f9ae8111f35556919
ngouiaa <gouiaanour45@gmail.com>
2024-11-03 23:23:44 +0100

    Update and rename Pseudo to pseudo.c

---
+++ pseudo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "pseudo.h"//
+// Created by gouia on 03/11/2024.
+//
+
+#include "pseudo.h"
+void_demanderdenterlepseudo(void)
+{
+    printf("enter votre nom de joueur\n");
+}
+void_afficherpseudo (char pseudo) {
+    printf(" %c .\n", pseudo );
+}
