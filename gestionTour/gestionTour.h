04abec8e5536fa839025c9835039d977a4318e5b
ngouiaa <gouiaanour45@gmail.com>
2024-11-20 22:57:23 +0100

    Update gestionTour.h

--- gestionTour/gestionTour.h
+++ gestionTour/gestionTour.h
@@ -1,8 +1,12 @@
-//
-// Created by gouia on 19/11/2024.
-//
 
-#ifndef GESTIONTOUR_H
-#define GESTIONTOUR_H
+#ifndef GESTION_TOUR_H
+#define GESTION_TOUR_H
 
-#endif //GESTIONTOUR_H
+
+void GestionTour(unsigned short int NBJoueurs,
+                 char NomJoueurs[4][32],
+                 unsigned short int TableauBarriereRest[4],
+                 unsigned short int TableauP[9][9],
+                 unsigned short int TableauB[8][8]);
+
+#endif
