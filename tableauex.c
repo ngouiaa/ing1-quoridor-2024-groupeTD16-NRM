13de3f068ad3d6b3bdbec57aa6168925b0530ed8
stnmateo <mateo.stengelin@edu.ece.fr>
2024-11-24 18:57:46 +0100

    Update tableauex.c

--- tableauex.c
+++ tableauex.c
@@ -1,8 +1,4 @@
-//
-// Created by rapha on 22/10/2024.
-//
-
-#include "tableauex.h"
+#include "Tableau.h"
 #include <stdio.h>
 void initialiser_plateau(char plateau[T][T], int nbjoueur) {
     int i,j;
@@ -44,27 +40,60 @@ void afficher_plateau(char plateau[T][T]) {
         printf("|\n");
     }
 }
-typedef struct {
-    int x;
-    int y;
-} Position;
 
-void deplacerPion(Position *pion, char direction) {
-    switch (direction) {
-        case 'z': pion->x--; break; // Haut
-        case 's': pion->x++; break; // Bas
-        case 'q': pion->y--; break; // Gauche
-        case 'd': pion->y++; break; // Droite
-        default: printf("Direction invalide !\n"); break;
+void nom_joueurs(int *nbjoueur,char noms[4][50]) {
+    do {
+        printf("jouer a 2 OU 4 :");
+        scanf("%d",nbjoueur);
+        if(*nbjoueur !=4 && *nbjoueur !=2) {
+            printf("Nombre invalide ! jouer a 2 OU 4 :");
+        }
+    }while(*nbjoueur!=4 && *nbjoueur!=2);
+    for(int i=0;i<*nbjoueur;i++) {
+        printf("Nom du joueur %d : ", i+1);
+        scanf("%s",noms[i]);
     }
-
-    // Vérification des limites du tableau
-    if (pion->x < 0) pion->x = 0;
-    if (pion->x >= T) pion->x = T - 1;
-    if (pion->y < 0) pion->y = 0;
-    if (pion->y >= T) pion->y = T - 1;
+    printf("\nListe des joueurs :\n");
+    for(int i=0; i<*nbjoueur; i++) {
+        printf("Joueur %d : %s\n", i+1,noms[i]);
+    }
 }
 
+void
 
+/* Initialise les joueurs avec le nombre de barrières selon le nombre de joueurs
+void initialiser_joueurs(Joueur joueurs[], int nb_joueurs) {
+    int barrières_par_joueur = (nb_joueurs == 2) ? MAX_BARRIERES_2_JOUEURS : MAX_BARRIERES_4_JOUEURS;
 
+    for (int i = 0; i < nb_joueurs; i++) {
+        joueurs[i].barrieres_restantes = barrières_par_joueur;
+        joueurs[i].nombre_barrieres_placees = 0;
+    }
+}
+
+// Vérifie si une barrière peut être placée à une position donnée
+int verifier_position_barriere(const Barriere *barriere, char grille[T][T]) {
+    if (barriere->x < 0 || barriere->x >= T - 1 ||
+        barriere->y < 0 || barriere->y >= T - 1) {
+        printf("Position invalide : hors des limites de la grille.\n");
+        return 0;
+        }
+
+    if (barriere->orientation == 'H') {
+        if (grille[barriere->x][barriere->y] != ' ' || grille[barriere->x][barriere->y + 1] != ' ') {
+            printf("Position invalide : conflit avec une barriere horizontale existante.\n");
+            return 0;
+        }
+    } else if (barriere->orientation == 'V') {
+        if (grille[barriere->x][barriere->y] != ' ' || grille[barriere->x + 1][barriere->y] != ' ') {
+            printf("Position invalide : conflit avec une barriere verticale existante.\n");
+            return 0;
+        }
+    } else {
+        printf("Orientation invalide. Utilisez 'H' ou 'V'.\n");
+        return 0;
+    }
+
+    return 1; // Position valide
+}*/
 
