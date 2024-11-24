450dbc49c17e0d7341f64f65b9f2d7141d424814
ngouiaa <gouiaanour45@gmail.com>
2024-11-24 15:23:09 +0100

    Update MAINN.c

--- MAIN/MAINN.c
+++ MAIN/MAINN.c
@@ -1,45 +1,61 @@
 //
 // Created by gouia on 20/11/2024.
 //
-#include <stdio.h>
+
 #include "MAINN.h"
-#include "AffichageHistorique.h"
-int main() {
-    printf("Bienvenue dans le jeu Quoridor !\n");
+#include <stdio.h>
+#include <string.h>
 
-    // Initialisation des structures de données
-    InitialiserStructures();
-
-    // Configuration du jeu : nombre de joueurs, noms, symboles, barrières
-    ConfigurerJeu();
-
-    // Afficher la configuration initiale
-    printf("\nConfiguration du jeu :\n");
-    printf("Nombre de joueurs : %d\n", NBJoueurs);
-    for (unsigned short int i = 0; i < NBJoueurs; i++) {
-        printf("Joueur %d : %s, Symbole: %d, Barrières restantes: %d\n",
-               i + 1, NomJoueurs[i], TableauSymbole[i], TableauBarriereRest[i]);
+void InitialiserStructures() {
+    memset(TableauP, 0, sizeof(TableauP));
+    memset(TableauB, 0, sizeof(TableauB));
+    memset(TableauHistorique, 0, sizeof(TableauHistorique));
+    for (unsigned short int i = 0; i < 4; i++) {
+        TableauBarriereRest[i] = 0;
+        OrdreJeu[i] = i;
+        TableauSymbole[i] = 0;
+        strcpy(NomJoueurs[i], "");
     }
-
-    // Simuler une séquence de jeu
-    printf("\nDébut du jeu :\n");
-    for (unsigned short int i = 0; i < NBJoueurs; i++) {
-        // Chaque joueur choisit une action
-        int action = ChoisirAction(i);
-
-        // Enregistrer l'action dans l'historique
-        EnregistrerHistorique(i, action);
-
-        // Afficher un résumé de l'action
-        if (action == 1) {
-            printf("Joueur %s a posé une barrière.\n", NomJoueurs[i]);
-        } else if (action == 2) {
-            printf("Joueur %s a déplacé son pion.\n", NomJoueurs[i]);
-        }
-    }
-
-    // Afficher l'historique des actions jouées
-    AfficherHistorique();
-
-    return 0;
+    NBJoueurs = 0;
 }
+
+void ConfigurerJeu() {
+    do {
+        printf("Entrez le nombre de joueurs (2 ou 4) : ");
+        scanf("%hu", &NBJoueurs);
+        if (NBJoueurs != 2 && NBJoueurs != 4) {
+            printf("Erreur : le nombre de joueurs doit être 2 ou 4.\n");
+        }
+    } while (NBJoueurs != 2 && NBJoueurs != 4);
+
+    for (unsigned short int i = 0; i < NBJoueurs; i++) {
+        printf("Entrez le nom du joueur %d : ", i + 1);
+        scanf("%s", NomJoueurs[i]);
+    }
+
+    unsigned short int barrieresParJoueur = (NBJoueurs == 2) ? 10 : 4;
+    for (unsigned short int i = 0; i < NBJoueurs; i++) {
+        TableauBarriereRest[i] = barrieresParJoueur;
+    }
+
+    for (unsigned short int i = 0; i < NBJoueurs; i++) {
+        TableauSymbole[i] = i + 1;
+    }
+}
+
+int ChoisirAction(unsigned short int joueur) {
+    int choix;
+    printf("Joueur %d, choisissez une action :\n", joueur + 1);
+    printf("1. Poser une barrière\n");
+    printf("2. Deplacer le pion\n");
+    printf("Entrez votre choix (1 ou 2) : ");
+    scanf("%d", &choix);
+
+    while (choix != 1 && choix != 2) {
+        printf("Choix invalide. Entrez 1 pour poser une barriere ou 2 pour deplacer le pion : ");
+        scanf("%d", &choix);
+    }
+    return choix;
+}
+
+
